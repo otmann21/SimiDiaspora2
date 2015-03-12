9bf9f2bf0619f919950db33fdff9a2d5115a7191
otmann21 <otmann.tnitan@gmail.com>
2015-03-12 17:51:12 +0100

    Methodes ajoutContenu et reponseContenu

--- src/processSuperPeer/GestionContenu.java
+++ src/processSuperPeer/GestionContenu.java
@@ -1,5 +1,8 @@
 package processSuperPeer;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 import org.simgrid.msg.Msg;
@@ -24,7 +27,7 @@ public class GestionContenu extends Process{
 	 * Dans la Hashmap de haut niveau, les clés sont les noms des utilisateurs.
 	 * Dans les petites Hashmaps, les clés sont des hash de publications, et les valeurs sont les publications.
 	 */
-	HashMap<String, HashMap<String, String>> contenu;
+	HashMap<String, HashMap<String, String>> donnees;
 
 	/**
 	 * La boite aux lettres sur laquelle le sp va écouter les requetes.
@@ -60,13 +63,46 @@ public class GestionContenu extends Process{
 		}
 	}
 
-	public void ajoutContenu(String Contenu, String expediteur){
+	public void ajoutContenu(String publication, String expediteur){
 
+		//On vérifie si on a déjà une entrée pour le peer qui veut poster, sinon on en crée une
+		if(!donnees.containsKey(expediteur)){
+			donnees.put(expediteur, new HashMap<String, String>());
+		}
+
+		//On calcule le hash de la publication
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("SHA-1");
+			md.reset();
+			md.update(publication.getBytes());
+			String hash;
+
+			try {
+				hash = new String(md.digest(), "UTF-8");
+
+				//on l'ajoute
+				donnees.get(expediteur).put(hash, publication);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		} catch (NoSuchAlgorithmException e1) {
+
+			e1.printStackTrace();
+		}
 	}
 
 	String reponseContenu(String posteur, String hash){
-
-		return null;
+		
+		String reponse=null;
+		
+		if(donnees.containsKey(posteur)){
+			if(donnees.containsKey(hash)){
+				reponse = donnees.get(posteur).get(hash);
+			}
+		}
+		
+		return reponse;
 	}
 
 }
