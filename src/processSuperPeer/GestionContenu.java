b517559f75dd0a229dcad7b6f76e152cd0cc7dd1
otmann21 <otmann.tnitan@gmail.com>
2015-03-12 15:23:25 +0100

    Creation GestionContenu

---
+++ src/processSuperPeer/GestionContenu.java
@@ -0,0 +1,23 @@
+package processSuperPeer;
+
+import org.simgrid.msg.Msg;
+import org.simgrid.msg.Process;
+import org.simgrid.msg.Host;
+import org.simgrid.msg.MsgException;
+
+public class GestionContenu extends Process{
+
+	/**
+	 * @param args
+	 */
+
+	public GestionContenu(Host host, String name, String[]args) {
+		super(host,name,args);
+	}
+
+	public void main(String[] args) throws MsgException {
+		
+
+	}
+
+}
---
+++ src/taches/typeMessage.java
@@ -0,0 +1,14 @@
+package taches;
+
+/**
+ * Tous les types utilisés par Message vont être créés ici.
+ * Ajouter des commentaires pour préciser leur rôle.
+ * 
+ *
+ * @author otmann
+ *
+ */
+
+public enum typeMessage {
+
+}
