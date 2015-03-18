28937546514bbc3e7bd162a01d2a3ee1303c7788
hugoedlinger <hugoh951@hotmail.com>
2015-03-18 14:12:39 +0100

    resolution d'une majorite de buggs

--- src/processPeer/Consulter.java
+++ src/processPeer/Consulter.java
@@ -46,13 +46,18 @@ public class Consulter extends Process{
 	/**recupere l'arraylist contenant le hache et le SPContenu de chaque publication.
 	 * Quand un peer demande à voir du contenu, on lui donne le contenu de son premier peer ami. 
 	 * celui-ci appelle la méthode recupere mur.
+	 * @throws TimeoutException 
+	 * @throws HostFailureException 
+	 * @throws TransferFailureException 
 	 */
-	public ArrayList <String[]> recupereMur(){		
-		
+	public ArrayList <String[]> recupereMur() throws TransferFailureException, HostFailureException, TimeoutException{		
+
 		// Quand on recupere le mur, on obtient un ensemble de couple (hache, contenu)
 		// On commence par demander à SP LiensAmis la liste de ses amis.
+		
+		ArrayList<String[]> listePubli = new ArrayList();
 
-		Message<String> recupListeAmis;
+		Message<String> recupListeAmis = new Message();
 		recupListeAmis.setType(typeMessage.liste_ami);
 		//recupListeAmis.setPeerConcerne(peer);
 		recupListeAmis.isend(this.SPAmi); //envoi du message au spWall, puis attente de sa réponse.
@@ -74,13 +79,13 @@ public class Consulter extends Process{
 				//on a choisi le peer a consulter, le peer 0, qui est ami avec tout le monde.
 			}	
 		}		
-		
+
 		//recherche du SPWall qui lui correspond.
-		Message<String> demande;
+		Message<String> demande = new Message();
 		demande.setType(typeMessage.demandeSPWall);
 		demande.setExpediteur(this.mbox);
 		demande.isend(this.SPAmi);
-		
+
 		//reception du SPWall
 		try {
 			Process.sleep(100); //si ce temps n'est pas suffisant, on ajoutera du temps.
@@ -96,59 +101,56 @@ public class Consulter extends Process{
 				//affectation du SPWall
 				this.spWall = spWallDuPeer;
 			}
-		}
-		
-		
-		// Demande de consultation du mur du premier de ses amis.
-		ArrayList<String[]> listePubli = new ArrayList();
 
-		Message<String> reqWall = new Message();	//creation du message de requete au SPWall.
-		reqWall.setType(typeMessage.requete_mur);
-		reqWall.setPeerConcerne(this.peer);
-		reqWall.isend(spWall); //envoi du message au spWall, puis attente de sa réponse.
 
-		try {
-			Process.sleep(100); //si ce temps n'est pas suffisant, on ajoutera du temps.
-		} catch (HostFailureException e) {
-			e.printStackTrace();
-		}
+			// Demande de consultation du mur du premier de ses amis.
 
-		if(Task.listen(mbox)){
-			Message msg= (Message) Task.receive(mbox);
-			boolean resultat = (msg.getType()==typeMessage.reponseMur) && (msg.getHashCodeMessagePrecedent()==reqWall.hashCode());
-			if (resultat){
-				listePubli = (ArrayList<String[]>) msg.getMessage();
+			Message<String> reqWall = new Message();	//creation du message de requete au SPWall.
+			reqWall.setType(typeMessage.requete_mur);
+			reqWall.setPeerConcerne(this.peer);
+			reqWall.isend(spWall); //envoi du message au spWall, puis attente de sa réponse.
+
+			try {
+				Process.sleep(100); //si ce temps n'est pas suffisant, on ajoutera du temps.
+			} catch (HostFailureException e) {
+				e.printStackTrace();
 			}
 
-			return listePubli ;
+			if(Task.listen(mbox)){
+				Message msg= (Message) Task.receive(mbox);
+				boolean resultat1 = (msg.getType()==typeMessage.reponseMur) && (msg.getHashCodeMessagePrecedent()==reqWall.hashCode());
+				if (resultat1){
+					listePubli = (ArrayList<String[]>) msg.getMessage();
+				}
+
+				
+			}
 		}
+		return listePubli;
 	}
 
 	public String recuperePubli(String hache){ //sp contenu attribut inutile
 
-			String SPContenu;
-			//On recupere le mur grace a la fonction precedente, caD la liste (hache, SPContenu).
-			
-			ArrayList mur2 = this.recupereMur();
-			String[] couple = recupereMur().get(mur2.indexOf(hache));
-			//on prend l'element de mur2 associé au message 'hache'
+		//Récupération du mur et du SPContenu de la publi.
+		ArrayList mur2 = this.recupereMur();
+		String[] couple = recupereMur().get(mur2.indexOf(hache));
+		String SPContenu = couple[1];
+		//On recupere le mur grace a la fonction precedente, caD la liste (hache, SPContenu).
+		//on prend l'element de mur2 associé au message 'hache', cad l'adresse du spContenu.
 
-			Message<String> demandePubli = new Message<String>();
-			demandePubli.setMessage("");
-			demandePubli.setPeerConcerne("");
-			demandePubli.setMboxReponse("");
-			// ces attributs sont a renseigner.
+		//creation et envoi du message à SPContenu
+		Message<String> demandePubli = new Message<String>();
+		demandePubli.setMessage(hache);
+		demandePubli.setPeerConcerne(this.peer);
+		demandePubli.setMboxReponse(this.mbox);
+		demandePubli.setType(typeMessage.requete_publication);
+		demandePubli.isend(SPContenu);
 
-			// on demande ensuite la liste au process gestion mur.
-			demandePubli.setType(typeMessage.requete_publication);
-			demandePubli.isend((String) indSp.next());
-
-			try {
-				Message<String> publication= (Message) Task.receive(mbox);
-				boolean res = (publication.getType()==typeMessage.reponse_publication) && (publication.getHashCodeMessagePrecedent()==demandePubli.hashCode());
-				if (res){
-					listePubli = (ArrayList<String[]>) msg.getMessage();
-				}
+		if(Task.listen(mbox)){
+			Message<String> publication= (Message) Task.receive(mbox);
+			boolean res = (publication.getType()==typeMessage.reponse_publication) && (publication.getHashCodeMessagePrecedent()==demandePubli.hashCode());
+			if (res){
+				publi = (String) msg.getMessage();
 
 
 				String[] el = (String[]) indSp.next();
@@ -156,52 +158,46 @@ public class Consulter extends Process{
 				listeContenu.add(spContenu);
 			}
 
-		} catch (TransferFailureException | HostFailureException
-				| TimeoutException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//recherche du hache de l'élement dans le mur, on cherche son indice.
+			Iterator i = mur2.iterator();
+			ArrayList listeHache = new ArrayList();
+
+			while (i.hasNext()){
+				String[] el = (String[]) i.next();
+				String elHach = el[0];
+				listeHache.add(elHach);
+			}
+
+			int indice = listeHache.indexOf(hache); //calcul de l'indice, en supposant qu'il est unique.
+			String[] couple = (String[]) listeHache.get(indice);
+			String publi = couple[1];
+			return publi ;
 		}
 
-		//recherche du hache de l'élement dans le mur, on cherche son indice.
-		Iterator i = mur2.iterator();
-		ArrayList listeHache = new ArrayList();
+		/**
+		 * consulterMur retourne toutes les publications du mur.
+		 * On appelle x fois la fonction recupere publi.
+		 * On n'envoie pas le resultat avec un message.
+		 * @param peer : le pair dont on consulte le mur.
+		 * @return la liste de toutes les publications du mur.
+		 */
+		public ArrayList <String> consulterMur(String peer){
+			ArrayList<String> liste = new ArrayList() ;
 
-		while (i.hasNext()){
-			String[] el = (String[]) i.next();
-			String elHach = el[0];
-			listeHache.add(elHach);
+			ArrayList mur = this.recupereMur();
+			Iterator it = mur.iterator();
+
+			while (it.hasNext()){
+				String[] el = (String[]) it.next();
+				String contenu = el[1];
+				liste.add(contenu);
+			}
+			return liste ;
 		}
 
-		int indice = listeHache.indexOf(hache); //calcul de l'indice, en supposant qu'il est unique.
-		String[] couple = (String[]) listeHache.get(indice);
-		String publi = couple[1];
-		return publi ;
-	}
+		public void main(String[] args) throws MsgException{
+			// TODO Auto-generated method stub
 
-	/**
-	 * consulterMur retourne toutes les publications du mur.
-	 * On appelle x fois la fonction recupere publi.
-	 * On n'envoie pas le resultat avec un message.
-	 * @param peer : le pair dont on consulte le mur.
-	 * @return la liste de toutes les publications du mur.
-	 */
-	public ArrayList <String> consulterMur(String peer){
-		ArrayList<String> liste = new ArrayList() ;
-
-		ArrayList mur = this.recupereMur();
-		Iterator it = mur.iterator();
-
-		while (it.hasNext()){
-			String[] el = (String[]) it.next();
-			String contenu = el[1];
-			liste.add(contenu);
 		}
-		return liste ;
-	}
-
-	public void main(String[] args) throws MsgException{
-		// TODO Auto-generated method stub
 
 	}
-
-}
